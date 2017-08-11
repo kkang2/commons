0a9de77bead80caa53e47a3345a56513548b6e6b
mallakkangyi <mallakkangyi@mobigen.com>
2017-08-11 14:23:38 +0900

    netty 예제 추가

---
+++ netty/src/main/java/com/psj/common/netty/exam/DiscardServerHandler.java
@@ -0,0 +1,57 @@
+package com.psj.common.netty.exam;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		// Discard the received data silently.
+		((ByteBuf)msg).release();
+	}
+	
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		// Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+	}
+	
+	public void run() throws Exception {
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		
+		try {
+			ServerBootstrap bs = new ServerBootstrap();
+			bs.group(bossGroup, workerGroup)
+				.channel(NioServerSocketChannel.class)
+				.childHandler(new ChannelInitializer<Channel>() {
+					@Override
+					protected void initChannel(Channel ch) throws Exception {
+						ch.pipeline().addLast(new DiscardServerHandler());
+					}
+				}).option(ChannelOption.SO_BACKLOG, 128)
+					.childOption(ChannelOption.SO_KEEPALIVE, true);
+			ChannelFuture future = bs.bind(9999).sync();
+			future.channel().closeFuture().sync();
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+		}
+	}
+	
+	public static void main(String args[]) throws Exception {
+		new DiscardServerHandler().run();
+	}
+}
