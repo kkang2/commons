5e64b4ab1e026f315ab3fa09193e6ab0cb1518c8
mallakkangyi <mallakkangyi@mobigen.com>
2017-09-11 15:34:01 +0900

    커밋

--- GoSampleProject/src/syntax/test.go
+++ GoSampleProject/src/syntax/test.go
@@ -2,86 +2,145 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os/exec"
-	"bytes"
-    "strings"
+    _"strings"
 )
 
-var CHECK_MAP_KEYS = []string{"Size"}
-
-func GetLineStrings(c string, arg ...string) ([]string, int) {
-    cmd := exec.Command(c, arg...)
-    var outb, errb bytes.Buffer
-
-    cmd.Stdout = &outb
-    cmd.Stderr = &errb
-
-    err := cmd.Run()
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    lineStrings := strings.SplitAfter(outb.String(), "\n")
-
-    return lineStrings, len(lineStrings)
-}
-
-func GetDmiData() []map[string]string {
-	lineStrings, _ := GetLineStrings("dmidecode", "--type", "memory")
-	dataMapList := []map[string]string{}
-	dataMap := map[string]string{}
-	
-	isMapData := false
-	
-	for _, line := range lineStrings {
-		line = strings.TrimSpace(line)
-		
-		//fmt.Println("line : " + line)
-		
-		if strings.Contains(line, "Memory Device") && len(line) == 13 {
-			isMapData = true
-			continue
-		} else if len(line) == 0 {
-			
-			if len(dataMap) > 0 && isValidMap(dataMap) {
-				dataMapList = append(dataMapList, dataMap)
-				dataMap = map[string]string{}
-			}
-			
-			isMapData = false
-			continue
-		}
-		
-		if isMapData && strings.Contains(line, ":") {
-			sp := strings.SplitN(line, ":", 2)
-			key := strings.TrimSpace(sp[0])
-			val := strings.TrimSpace(sp[1])
-			
-			dataMap[key] = val
-		}
-	}
-	
-	if len(dataMap) > 0 && isValidMap(dataMap) {
-		dataMapList = append(dataMapList, dataMap)
-	}
-	
-	return dataMapList
-}
-
-func isValidMap(data map[string]string) bool {
-	for _, key := range CHECK_MAP_KEYS {
-		val, ok := data[key]
-		
-		if !ok || strings.ToLower(val) == "no module installed" {
-			return false
-		}
-	}
-	
-	return true
-}
-
 func main() {
-	fmt.Println(GetDmiData())
-	fmt.Println(len(GetDmiData()))
+	var namespace []string
+	tt := []string{"skt", "hwinfo", "disk"}
+	
+	namespace = append(namespace, tt...)
+	
+	fmt.Println(namespace)
+	
+	//args := strings.Split("-d megaraid,0 /dev/sda", " ")
+	
+	
+	//fmt.Println(strings.Split("megaraid,0", ","))
+	/*
+	if len(args) == 3 {
+		fmt.Println(args[0])
+		fmt.Println(strings.Split(args[2][1:], "/"))
+		fmt.Println(args[2])
+	}
+	*/
+	//args = append(args, strings.Split("-d megaraid,0 /dev/sda", " ")...)
+	
+	//fmt.Println(args)
+	
+	//arg(args...)
 }
+
+func arg(arg ...string) {
+	fmt.Println("==== 값 출력 ====")
+	for _, val := range arg {
+		fmt.Println(val)
+	}
+	
+	fmt.Println(len(arg))
+}
+
+/*
+var lines = []string{
+		"/dev/sda -d scsi # /dev/sda, SCSI device",
+	}
+	
+	devNames := make([]string, len(lines))
+    numDev := 0
+    
+    diskName := ""
+    raidCount := -1
+	
+	for _, line := range lines {
+		 if !strings.Contains(line, "/dev") {
+	         continue
+		 }
+        
+        if index := strings.Index(line, " -d scsi "); index > -1 {
+	        if  raidCount == 0 {
+	        	if  numDev == 0 { // raid 장비가 아니고 두번째 디바이스 라는 뜻
+		        	devNames[numDev] = diskName
+	                numDev++
+	        	}
+	        	
+		        devNames[numDev] = line[0:strings.Index(line, " -d scsi ")]
+                numDev++
+	        }
+	        
+	        diskName = line[0:index]
+	        raidCount = 0
+        } else {
+	        if diskName != "" {
+	        	str := "-d " + line[strings.Index(line, " -d ")+4:strings.Index(line, " # ")] + " " + diskName
+	        	
+	        	//fmt.Println(str)
+	        	
+		        devNames[numDev] = str
+                numDev++
+                raidCount++
+	        }
+        }
+	}
+	
+	if diskName != "" && numDev == 0 {
+		fmt.Println(diskName)
+	}
+	
+	//fmt.Println(devNames)
+	fmt.Println(numDev)
+	
+	for _, val := range devNames {
+		fmt.Println(val)
+	}
+	
+	fmt.Println("끝")
+	
+	//var str = "-d megaraid,0 /dev/sda"
+	var str = "/dev/sda"
+	
+	fmt.Println(len(strings.Split(str, " ")))
+	fmt.Println(strings.Split(str, " "))
+	
+	for _, val := range strings.Split(str, " ") {
+		fmt.Println("val : " + val)
+	}
+	
+	var tt = make([]string, 5)
+	
+	tt[0] = "-i"
+	tt[1] = "/dev/sda"
+	tt[2] = "/dev/sdb"
+	
+	arg(tt[0:len(tt)]...)
+	
+	/*
+	var commands = []string{}
+	
+	fmt.Println(len(commands))
+	
+	commands[0] = "dsfsdf"
+	
+	fmt.Println(len(commands))
+	*/
+	
+	/*
+	for _, line := range lines {
+		index := strings.Index(line, " -d scsi ")
+		
+		if strings.Contains(line, " -d scsi ") {
+			fmt.Println(index)
+			fmt.Println(line)
+			fmt.Println(line[0:index])
+		} else {
+			index = strings.Index(line, " # ")
+			fmt.Println(index)
+			fmt.Println(line)
+			fmt.Println(line[strings.Index(line, " -d ")+4:strings.Index(line, " # ")])
+		}
+		
+		fmt.Println("")
+		
+		//fmt.Println(line)
+	}
+
+*/
