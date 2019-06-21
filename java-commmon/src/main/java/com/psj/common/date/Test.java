36caf13a1f872abb88092dfe40f565f73eb62fb4
sanghya <sanghya@gmail.com>
2019-06-21 19:22:15 +0900

    - java-common 프로젝트 최초커밋

---
+++ java-commmon/src/main/java/com/psj/common/date/Test.java
@@ -0,0 +1,12 @@
+package com.psj.common.date;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Test {
+
+    public static void main(String[] args) {
+        log.info("dfsdf");
+    }
+
+}
