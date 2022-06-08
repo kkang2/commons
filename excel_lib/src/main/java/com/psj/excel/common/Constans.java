d9f07bf4d893146221fce1cd03bb8c5abd7ed518
user <user@LAPTOP-U0CO45L6>
2022-06-08 11:13:39 +0900

    init commit

---
+++ excel_lib/src/main/java/com/psj/excel/common/Constans.java
@@ -0,0 +1,5 @@
+package com.psj.excel.common;
+
+public class Constans {
+	public static String CREATE_FOLDER = "C:\\Users\\user\\program\\eclipse\\workspace-eclipse-2021-12\\excel_lib\\src\\main\\resources\\excel\\create";
+}
